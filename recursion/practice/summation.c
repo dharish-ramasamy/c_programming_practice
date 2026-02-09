26bf56e4670418c37094af812786fd7591bfc3c6
dharish-ramasamy <dharishjayakodi@gmail.com>
2026-02-09 06:30:30 +0000

    problem : summation of number from 1 to n uploaded

---
+++ recursion/practice/summation.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+int summation(int);
+
+int main(void)
+{
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    if(num <= 0)
+    {
+        printf("Enter positive number.\n");
+    }
+    else
+    {
+        printf("Summation of numbers from 1 to %d is %d.\n", num, summation(num));
+    }
+    return 0;
+}
+
+int summation(int n)
+{
+    if(n == 0)
+    {
+        return 0;
+    }
+    return (n + summation(n-1));
+}
