5b0f462b90c82b3b29939b965d11d1936aadad6c
dharish-ramasamy <dharishjayakodi@gmail.com>
2026-02-09 06:37:57 +0000

    problem : displaying number from 1 to n uploaded

---
+++ recursion/practice/display.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+int display(int);
+
+int main(void)
+{
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    if(num < 0)
+    {
+        printf("Enter positive integer.\n");
+        return 0;
+    }
+    display(num);
+    printf("\n");
+    return 0;
+}
+
+int display(int n)
+{
+    if(n == 0)
+    {
+        return 0;
+    }
+    display(n-1);
+    printf(" %d ", n);
+}
