96f350abcbbe22a0b87a49c0bd07d6725354c49d
dharish-ramasamy <dharishjayakodi@gmail.com>
2026-02-11 16:14:09 +0000

    preprocessor and escape sequence mcq uploaded

---
+++ c_mcq_practice/q71.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#define message "pointer is\ power of c"
+
+int main(void)
+{
+    printf("%s", message);
+    return 0;
+}
+
+/*
+ * What is the output?
+ * Ans: pointer is power of c
+ */
