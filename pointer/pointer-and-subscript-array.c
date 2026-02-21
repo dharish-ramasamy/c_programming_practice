0919eb2b96200d1b37fd7220631231925e861bbd
dharish-ramasamy <dharishjayakodi@gmail.com>
2026-02-21 13:35:30 +0000

    problem: elements of array by pointer & subscript notation

---
+++ pointer/pointer-and-subscript-array.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(void)
+{
+    int array[5] = {5, 10, 15, 20,25};
+    for(int i = 0; i < 5; i++)
+    {
+        printf("Value of array[%d] = ", i);
+        printf("%d\t", array[i]);
+        printf("%d\t", *(array+i));
+        printf("%d\t", *(i+array));
+        printf("%d\n", i[array]);
+        printf("Address of array[%d] = %p\n", i, array+i);
+    }
+    return 0;
+}
