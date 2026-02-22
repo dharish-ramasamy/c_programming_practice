55e402059a875a5dd1469e04da48260b4ec27808
dharish-ramasamy <dharishjayakodi@gmail.com>
2026-02-22 04:10:56 +0000

    problem: Dynamic Memory Allocation - calloc()

---
+++ pointer/dynamic-calloc.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(void)
+{
+    int n, *ptr;
+    printf("Enter total number of integers: ");
+    scanf("%d", &n);
+    
+    ptr = (int *)calloc(n, sizeof(int)); //initialize allocated memory to zero
+    if(ptr == NULL)
+    {
+        printf("Memory not available..\n");
+        exit(1);
+    }
+    
+    printf("Enter integer: ");
+    for(int i = 0; i < n; i++)
+    {
+        scanf("%d", ptr+i);
+    }
+
+    printf("Entered value:");
+    for(int i = 0; i < n; i++)
+    {
+        printf(" %d", *(ptr+i));
+    }
+    printf("\n");
+    return 0;
+}
