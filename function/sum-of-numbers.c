6614bf6ecec97c6867c97d41781383cc60c6683a
dharish-ramasamy <dharishjayakodi@gmail.com>
2026-02-25 15:32:13 +0000

    problem: sum of two numbers

--- function/sum-of-numbers.c
+++ function/sum-of-numbers.c
@@ -7,8 +7,13 @@ int main(void)
     int x, y, total;
     printf("Enter values for x and y : ");
     scanf("%d%d", &x, &y);
-    total = sum(x,y); // function call
+    total = sum(x, y); // function call
     printf("Sum of %d and %d is %d\n", x, y, total);
+    total = sum(4, 5); // function call
+    printf("Sum of 4 and 5 is %d\n", total);
+    total = sum(x+y, y*2); // function call
+    printf("Sum of %d and %d is %d\n", x+y, y*2, total);
+
     return 0;
 }
 
