b30de1b206a382719e5f4eae18329adde0fea926
dharish-ramasamy <dharishjayakodi@gmail.com>
2026-02-25 16:18:45 +0000

    problem: factorial of a number

---
+++ function/factorial.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+
+long int factorial(int);
+
+int main(void)
+{
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    if(num < 0)
+    {
+        printf("No factorial for negative number.\n");
+    }
+    else
+    {
+        printf("Factorial of %d is %ld\n", num, factorial(num));
+    }
+    return 0;
+}
+
+long int factorial(int n)
+{
+    long int fact = 1;
+    if(n == 0)
+    {
+        return 1;
+    }
+    for(int i = 1; i <= n; i++)
+    {
+        fact *= i;
+    }
+    return fact;
+}
