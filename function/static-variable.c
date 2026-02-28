af816cbf829e30261be15d3d889792bb89266925
dharish-ramasamy <dharishjayakodi@gmail.com>
2026-02-28 06:58:43 +0000

    problem: Behaviour of static variable between function calls

---
+++ function/static-variable.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+void func();
+
+int main(void)
+{
+    func();
+    func();
+    func();
+    func();
+    return 0;
+}
+
+void func(void)
+{
+    int a = 10;
+    static int b = 10;
+    printf("a = %d, b = %d\n", a, b);
+    a++;
+    b++;
+}
